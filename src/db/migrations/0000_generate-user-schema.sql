84b577c91ebfa9d89dc8927fdd7e854cae815d4b
SHUMBUSHO Irumva <102556884+kent250@users.noreply.github.com>
2025-10-25 00:18:42 +0200

    feat: Create user schema
    create users schema and migration files

---
+++ src/db/migrations/0000_generate-user-schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"collection_name" varchar,
+	"username" varchar NOT NULL,
+	"email" varchar NOT NULL,
+	"password" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username"),
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "users_password_unique" UNIQUE("password")
+);
