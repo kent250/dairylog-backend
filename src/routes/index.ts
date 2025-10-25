7bce5594f37e29d55d87b8f8a9face9ac65e2b10
SHUMBUSHO Irumva <102556884+kent250@users.noreply.github.com>
2025-10-25 09:40:20 +0200

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { specs, swaggerUi } from '../config/swagger-doc.js';
 import authRoutes from "./auth.routes.js";
+import userRoutes from "./user.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 
 router.use('/doc', swaggerUi.serve, swaggerUi.setup(specs));
+
+router.use("/users", userRoutes);
+
 
 
 export default router;
