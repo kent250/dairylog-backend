52a9130d2c9e4c8b3fb8920510368b2eebb06339
SHUMBUSHO Irumva <102556884+kent250@users.noreply.github.com>
2025-10-25 16:35:08 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import { AppError } from "./utils/error-utils/AppError.js";
 import { ERROR_CODES } from "./utils/error-utils/errorCodes.js";
 
 const PORT = config.port;
+const HOST = '0.0.0.0';
 
 if (!PORT) {
     throw new AppError(ERROR_CODES.SERVER_PORT_MISSING);
 }
 
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
     console.log(`🚀 Server running in ${(config.environment ?? 'development').toUpperCase()} mode`);
     console.log(`🌐 URL: ${config.api.baseUrl}`);
     console.log(`📡 Port: ${PORT}`);
